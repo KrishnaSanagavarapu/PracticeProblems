e6582e9c41f74e489fcb14f32fd4aa0391cb9e8e
unknown <krishna.javadev12@gmail.com>
2019-12-16 20:46:25 -0500

    save

---
+++ Angular_practice/http-error-handling/src/app/employee-detail/employee-detail.component.ts
@@ -0,0 +1,25 @@
+import { EmployeeService } from './../employee.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'employee-detail',
+  template: `
+    <h2>Employee Detail</h2>
+    <ul *ngFor="let employee of employees">
+      <li>{{employee.employeeId}}. {{employee.firstName}} - {{employee.employeeStatus}}</li>
+    </ul>
+  `,
+  styles: []
+})
+export class EmployeeDetailComponent implements OnInit {
+
+  public employees = [];
+
+  constructor(private _employeeService:EmployeeService) { }
+
+  ngOnInit() {
+  
+
+  }
+
+}
